0f06af7e34b07865c0d00d227c92d0e5f882361b
David Colattie <davidcolatti@gmail.com>
2020-03-17 09:18:34 -0400

    Mumbling Kata

---
+++ Mumbling.js
@@ -0,0 +1,33 @@
+// Mumbling
+
+/* 
+Link: https://www.codewars.com/kata/mumbling/train/javascript
+Instruction: 
+This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+The parameter of accum is a string which includes only letters from a..z and A..Z. */
+
+
+function accum(s) {
+    let newSArr = [];
+    
+    for (let i = 0; i < s.length; i++){
+      newSArr.push(format(s[i], i + 1));
+    }
+    
+    return newSArr.join('-');
+  }
+  
+  function format(s, num){
+    let letter = s.toUpperCase();
+    
+    while (letter.length !== num){
+      letter += s.toLowerCase();
+    }
+    return letter;
+  }
