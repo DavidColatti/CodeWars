1fab2ab21da0ed37f45572e8de0674656951dd35
David Colattie <davidcolatti@gmail.com>
2020-03-16 20:24:04 -0400

    Exes & Ohs

---
+++ Exes&Ohs.js
@@ -0,0 +1,29 @@
+// Exes and Ohs
+
+/* 
+Link: https://www.codewars.com/kata/exes-and-ohs/train/javascript
+Instruction: 
+Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false */
+
+function XO(str) {
+    let xLetter = 0;
+    let oLetter = 0;
+    
+    for (let i = 0; i < str.length; i++)
+      if (str[i] === 'x' || str[i] === 'X') {
+        xLetter++;
+      } else if (str[i] === 'o' || str[i] === 'O') {
+        oLetter++;
+      }
+    return xLetter === oLetter;
+  }
+  
+  
